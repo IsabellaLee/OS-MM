2f1db1d1de784ae8c1349b8f8ae4fe53e9f23b81
Loslylee <lin.isabella.li@gmail.com>
2017-02-26 20:28:23 -0500

    Initial Commit

---
+++ pa2.cpp
@@ -0,0 +1,12 @@
+//
+//  pa2.cpp
+//  ProjectAssign#2
+//
+//  Created by Lin Li on 2/25/17.
+//  Copyright © 2017 Isabella Lee. All rights reserved.
+//
+
+#include "pa2.hpp"
+#include <iostream>
+using namespace std;
+
---
+++ pa2.hpp
@@ -0,0 +1,14 @@
+//
+//  pa2.hpp
+//  ProjectAssign#2
+//
+//  Created by Lin Li on 2/25/17.
+//  Copyright © 2017 Isabella Lee. All rights reserved.
+//
+
+#ifndef pa2_hpp
+#define pa2_hpp
+
+#include <stdio.h>
+
+#endif /* pa2_hpp */

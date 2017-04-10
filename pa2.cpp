486cc5490a5ff1e786015a71bcf3227785499a66
Loslylee <lin.isabella.li@gmail.com>
2017-04-10 10:49:36 -0400

    First Commit

--- pa2.cpp
+++ pa2.cpp
@@ -1,12 +1,110 @@
-//
-//  pa2.cpp
-//  ProjectAssign#2
-//
-//  Created by Lin Li on 2/25/17.
-//  Copyright © 2017 Isabella Lee. All rights reserved.
-//
-
 #include "pa2.hpp"
 #include <iostream>
+#include <string>
 using namespace std;
 
+int main(int argc, char ** argv) {
+    
+    //arg[1] is the parameter for which algorithm to use,
+    //best fit or worst fit
+    string algorithm = argv[1];
+    
+    Memory* memory = new Memory();
+    
+    //Indicate which algorithm to use at the top
+    if (algorithm == "best") {
+        cout << "Using best fit algorithm.\n" << endl;
+    }
+    else if (algorithm == "worst") {
+        cout << "Using worst fit algorithm.\n" << endl;
+    }
+    else {
+        //Data validation: if input is neither best or worst
+        //program will be terminated
+        cout << "Invalid Input.\n" << endl;
+        exit(0);
+    }
+    
+    //Print out the main menu
+    //Main menu includes all the operations provided to the user
+    cout << "1. Add program\n" << "2. Kill program\n" << "3. Fragmentation\n" << "4. Print memory\n" << "5. Exit\n" << endl;
+    
+    while (true) {
+        
+        //Integer choice is used to store the information about
+        //which actions to take. For example, if choice is 1, it means
+        //an add program action will be taken
+        
+        int choice;
+        cout << "choice - ";
+        
+        //Data validation: if user's input is not an integer,
+        //it will not be read into the cin, therefore user needs
+        //to try again until it pass the data validation test
+        
+        while(!(cin >> choice)){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout << "Invalid input. Try again.\n " << endl;
+            cout << "choice - ";
+        }
+        
+        //Implement the corresponding function based on the user's choice
+        switch (choice) {
+            //Add a program if case is 1
+            case 1: {
+                
+                //Read in both the program's name and size
+                cout << "Program name - ";
+                string programName;
+                cin >> programName;
+                cin.clear();
+                cout << "Program size (KB) - ";
+                int programSize;
+                cin >> programSize;
+                cin.clear();
+                
+                printf("\n");
+        
+                if (algorithm == "worst")
+                    memory->worstFitAlgorithm(programName, programSize);
+                else if (algorithm == "best")
+                    memory->bestFitAlgorithm(programName, programSize);
+            }
+                break;
+            
+            //Kill a program if case is 2
+            case 2: {
+                cout << "Program name - ";
+                string programName;
+                cin >> programName;
+                cin.clear();
+                cout << endl;
+                memory->kill(programName);
+            }
+                break;
+                
+            //Show the number of fragments if case is 3
+            case 3:
+                printf("\n");
+                printf("There are %d fragment(s).\n\n", memory->fragments());
+                break;
+             
+            //Print out how the memory is allocated by now if case is 4
+            case 4:
+                printf("\n");
+                memory->printMemory();
+                break;
+            
+            //Exit the program if case is 5
+            case 5:
+                exit(0);
+                
+            //Otherwise, enter another integer to pass the data validation
+            default:
+                printf("\n");
+                cout << "Invalid input, try again.\n" << endl;
+                break;
+        }
+    }
+}
--- pa2.hpp
+++ pa2.hpp
@@ -1,14 +1,266 @@
-//
-//  pa2.hpp
-//  ProjectAssign#2
-//
-//  Created by Lin Li on 2/25/17.
-//  Copyright © 2017 Isabella Lee. All rights reserved.
-//
-
 #ifndef pa2_hpp
 #define pa2_hpp
 
-#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//Page represents a chunk of memory
+class Page {
+public:
+    string name;
+    Page *next;
+    
+    Page(string name) : name(name) {
+        next = NULL;
+    }
+};
+
+
+//Memory represents the whole place where operation systems use to allocate
+//requested processes.
+//Page to Memory is analogous to Node to Linkedlist.
+
+class Memory {
+public:
+    
+    //Head is like the head node of a linkedlist, which indicates where
+    //the list starts. Here, it points to the start position of memory space
+    Page *head;
+    
+    //Constructor of the class memory
+    Memory() {
+        
+        head = new Page("Free");
+        Page *crt = head;
+      
+        int i = 1;
+        while (i < 32) {
+            crt->next = new Page("Free");
+            crt = crt->next;
+            i++;
+        }
+    }
+    
+    /*  Implement Best-Fit Strategy :
+        Find the start position of the smallest hole that is big enough */
+    
+    void bestFitAlgorithm(string programName, int programSize) {
+        
+        //smallestHole function here is used to return the start position to put the newly-requested program. if such position does not exist, return -1 instead
+        Page* start = smallestHole(programName, programSize);
+            
+        //Store the  value of how many pages are needed in variable pagesUsed
+        int pagesUsed = getSize(programSize);
+        
+        if (start) {
+            //Calculate the end position to put the program
+            Page* end = start;
+            int i = 0;
+            while (i <  getSize(programSize) && end != NULL) {
+                end = end->next;
+                i++;
+            }
+            
+            //Allocate the program based its start and end position
+            allocation(programName, start, end);
+            printf("Program %s added successfully: %d page(s) used.\n\n", programName.c_str(), pagesUsed);
+        }
+    }
+    
+    /*  Implement Worst-Fit Strategy :
+        Find the start position of the smallest hole that is big enough */
+    
+    void worstFitAlgorithm(string programName, int programSize) {
+        
+        //largestHole function here is used to return the start position to put the newly-requested program. if such position does not exist, return NULL instead
+        Page* start = largestHole(programName, programSize);
+        
+        //Store the  value of how many pages are needed in variable pagesUsed
+        int pagesUsed = getSize(programSize);
+        
+        if (start) {
+            //Calculate the end position to put the program
+            Page* end = start;
+            int i = 0;
+            while (i < getSize(programSize) && end != NULL){
+                end = end->next;
+                i++;
+            }
+            
+            //Allocate the program based its start and end position
+            allocation(programName, start, end);
+            printf("Program %s added successfully: %d page(s) used.\n\n", programName.c_str(), pagesUsed);
+        }
+    }
+    
+    //Kill program
+    void kill(string programName) {
+        Page *crt = head;
+        int cnt = 0;
+        
+        //Traverse the whole memory system by moving the crt pointer one step per time
+        while (crt) {
+            if (programName == crt->name) {
+                crt->name = "Free";
+                cnt++;
+            }
+            crt = crt->next;
+        }
+        
+        //if cnt is 0, it means the programName is not found during the traverse process of the whole memory system
+        if (cnt == 0) {
+            cout << "No such program to kill.\n" << endl;
+            return;
+        }
+        
+        printf("Program %s successfully killed, %d page(s) reclaimed.\n\n", programName.c_str(), cnt);
+        
+    }
+    
+    //calculate how many fragments are out there, and return it
+    int fragments() {
+        int cnt = 0;
+        Page* crt = head;
+        
+        while(crt) {
+            if (crt->name == "Free") {
+                do{
+                    //Crt pointer moves to next until it is null or not free
+                    crt = crt->next;
+                }while (crt != NULL && crt->name == "Free");
+                
+                cnt++;
+            }
+            if (crt) crt = crt->next;
+        }
+        return cnt;
+    }
+    
+    //Print memory
+    void printMemory() {
+        Page *crt = head;
+        while (crt) {
+            for (int i = 0; i < 8; i++) {
+                printf("%-6s", crt->name.c_str());
+                crt = crt->next;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+  
+//Functions below are only for internal use, therefore are private
+private:
+    
+    //Get the number of pages needed to allocate the program
+    int getSize(int programSize) {
+        return programSize % 4 == 0 ? programSize / 4 : programSize / 4 + 1;
+    }
+    
+    //Return the start position of the smallest hole that is large enough to put a program based on best-fit strategy
+    
+    Page* smallestHole(string programName, int programSize) {
+        if (duplicate(programName)) {
+            printf("Error, Program %s already running.\n\n", programName.c_str());
+            return NULL;
+        }
+        
+        int min = 33;
+        Page* start = NULL;
+        Page* crt = head;
+        int programPages = getSize(programSize);
+    
+        while (crt) {
+            int cnt = 0;
+            
+            if (crt->name == "Free") {
+                Page* tmp = crt;
+
+                do {
+                    cnt++;
+                    crt = crt->next;
+                } while (crt != NULL && crt->name == "Free");
+            
+            
+                if (cnt >= programPages && cnt < min) {
+                    start = tmp;
+                    min = cnt;
+                }
+            }
+            
+            if(crt) crt = crt->next;
+        }
+        
+        if (min == 33) {
+            printf("Error, Not enough memory for Program %s.\n\n", programName.c_str());
+            return NULL;
+        }
+        
+        return  start;
+    }
+
+    //Return the start position of the largest hole that is big enough to put the program based on worst-fit strategy
+    
+    Page* largestHole(string programName, int programSize) {
+        if (duplicate(programName)) {
+            printf("Error, Program %s already running.\n\n", programName.c_str());
+            return NULL;
+        }
+       
+        int max = 0;
+        Page* start = NULL;
+        Page* crt = head;
+        int programPages = getSize(programSize);
+        
+        while (crt)  {
+            int cnt = 0;
+          
+            if(crt->name == "Free") {
+              
+                Page* tmp = crt;
+                do {
+                    cnt++;
+                    crt = crt->next;
+                } while (crt != NULL && crt->name == "Free");
+            
+            
+                if (cnt >= programPages && cnt > max) {
+                    start = tmp;
+                    max = cnt;
+                }
+            }
+            if (crt) crt = crt->next;
+        }
+        
+        if (max == 0) {
+            printf("Error, Not enough memory for Program %s.\n\n", programName.c_str());
+            return NULL;
+        }
+         return  start;
+    }
+
+    //Allocate the program
+    void allocation(string programName, Page* start, Page* end) {
+        
+            while (start != end && start != NULL) {
+                start->name = programName;
+                start = start->next;
+        }
+    }
+    
+    //Check if the program is already added
+    bool duplicate(string programName) {
+        Page* crt = head;
+        while (crt) {
+            if (crt->name == programName) return true;
+            crt = crt->next;
+        }
+        return false;
+    }
+    
+};
+
 
 #endif /* pa2_hpp */
